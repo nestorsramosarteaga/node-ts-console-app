32a73c9e868a341d782b04216c2ce277f11d8d46
Nestor Ramos <nestorsramosarteaga@gmail.com>
2024-08-09 09:40:42 -0500

    Update App Test

--- test/app.test.ts
+++ test/app.test.ts
@@ -1,9 +1,37 @@
+// process.argv = ['node','app.ts','-b','8'];
+// import '../src/app';
+import { ServerApp } from '../src/presentation/server-app';
 
+describe('Test App.ts', ()=>{
 
-describe('App', ()=>{
+  test('should call Server.Run with values', async () => {
+    
+    const serverRunMock = jest.fn();
+    ServerApp.run = serverRunMock;
+    process.argv = [
+      'node','app.ts',
+      '-b','8',
+      '-l','20',
+      '-s',
+      '-n','test-filename',
+      '-d','test-outputs'
+    ];
 
-  test('should be true', ()=>{
-    expect(true).toBe(true);
+    // ACT
+    await import('../src/app');
+
+    // ASSERT
+    expect(serverRunMock).toHaveBeenCalledWith({
+      base: 8,
+      limit: 20,
+      showTable: true,
+      fileName: 'test-filename',
+      fileDestination: 'test-outputs'
+    });
+
+    // Reset Mock
+    serverRunMock.mockClear();
+
   });
 
 });
